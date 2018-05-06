6f0c5eea374e351055c1d1169302f92af42bf67b
cohki0305 <cohki0305@gmail.com>
2018-05-06 20:55:09 +0900

    generate auth token

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,10 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require('mongoose')
+const validator = require('validator')
+const { SHA256 } = require('crypto-js')
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -28,6 +31,27 @@ const User = mongoose.model('User', {
       required: true
     }
   }]
-});
+})
+
+UserSchema.methods.toJSON = function() {
+  const user = this
+  const userObject = user.toObject()
+
+  return _.pick(userObject, ['_id', 'email'])
+}
+
+UserSchema.methods.generateAuthToken = function () {
+  const user = this
+  const access = 'auth'
+  const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString()
+
+  user.tokens = user.tokens.concat({access, token})
+
+  return user.save().then(() => {
+    return token
+  })
+}
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = {User}

63bea8e04b8f132f401cd515f094dbdafc7ac8c9
cohki0305 <cohki0305@gmail.com>
2018-05-07 09:09:12 +0900

    save crypted password

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const validator = require('validator')
 const { SHA256 } = require('crypto-js')
 const jwt = require('jsonwebtoken')
 const _ = require('lodash')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -68,6 +69,21 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   })
 }
+
+UserSchema.pre('save', function(next) {
+  const user = this
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash
+        next()
+      })
+    })
+  } else {
+    next()
+  }
+})
 
 const User = mongoose.model('User', UserSchema);
 
